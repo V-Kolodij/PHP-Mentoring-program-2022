a876d1dcc4e17c6c1ba0e865b7d07fdd67012c12
Volodymyr.Kolodii@wideorbit.com <Volodymyr.Kolodii@wideorbit.com>
2022-07-18 23:48:12 +0300

    SOLID

--- public/solid.php
+++ public/solid.php
@@ -18,3 +18,56 @@ function what_list_am_i_on(array $actions): string {
     }
     return $naughty >= $nice ? 'naughty' : 'nice';
 }
+
+
+https://www.codewars.com/kata/57e8f757085f7c7d6300009a/php
+function planeSeat($a){
+
+    $num = (integer)$a;
+    $letter = substr($a, -1, 1);
+
+    if(!$num || !$letter || !validateData($num, $letter)){
+        return "No Seat!!";
+    }
+
+    return getNumPosition($num) . "-" . getLetterPos($letter);
+}
+
+function getNumPosition($num){
+    switch(true){
+        case $num <= 20:
+            $position = 'Front';
+            break;
+        case $num <= 40:
+            $position = 'Middle';
+            break;
+        case $num <= 60:
+            $position = 'Back';
+            break;
+    }
+    return $position;
+}
+
+function getLetterPos($letter){
+    $letterNum = ord($letter);
+    switch(true){
+        case $letterNum >= ord('G') && $letterNum <= ord('K'):
+            $position = 'Right';
+            break;
+        case $letterNum >= ord('D') && $letterNum <= ord('F'):
+            $position = 'Middle';
+            break;
+        case $letterNum >= ord('A') && $letterNum <= ord('C'):
+            $position = 'Left';
+            break;
+    }
+    return $position;
+
+}
+
+function validateData($number, $let) {
+    if($number > 60 || in_array($let, ['I', 'J']) || ord($let) > ord('K')){
+        return false;
+    }
+    return true;
+}
